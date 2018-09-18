b7d069049157e5a12296fb0aaa3a3d14891f49e3
Dan <39123300+dvpfig@users.noreply.github.com>
2018-09-18 23:52:54 +0200

    Added soco library and handlers to play/pause

--- __init__.py
+++ __init__.py
@@ -1,14 +1,31 @@
-from mycroft import MycroftSkill, intent_file_handler
+import soco
+from adapt.intent import IntentBuilder
+from mycroft import MycroftSkill, intent_handler
+from mycroft.audio import wait_while_speaking
 
 
 class SonosTmp(MycroftSkill):
+    """
+    Control Sonos speaker devices
+    
+    Use play/pause to control sonos device. 
+    """
+    
     def __init__(self):
-        MycroftSkill.__init__(self)
+        super(SonosTmp, self).__init__(name="SonosTmp")
+        self.zone_list = list(soco.discover())
 
-    @intent_file_handler('tmp.sonos.intent')
-    def handle_tmp_sonos(self, message):
-        self.speak_dialog('tmp.sonos')
+    # original: @intent_file_handler('tmp.sonos.intent')
+    @intent_handler(IntentBuilder("PlaySonos").require("Play").require("Sonos"))
+    def handle_play_sonos(self, message):
+        self.zone_list.play()
+        self.speak_dialog('play.sonos')
 
+        
+    @intent_handler(IntentBuilder("PauseSonos").require("Pause").require("Sonos"))
+    def handle_pause_sonos(self, message):
+        self.zone_list.pause()
+        self.speak_dialog('pause.sonos')
 
 def create_skill():
     return SonosTmp()
